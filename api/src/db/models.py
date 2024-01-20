357b34d92076cc41b021e35d9ed24c549e1a15cd
kylee <kyleetd@students.cs.ubc.ca>
2024-01-20 13:00:21 -0800

    Added SUQLAchemy to peotry env.

--- api/src/db/models.py
+++ api/src/db/models.py
@@ -6,3 +6,10 @@ from .database import Base
 class Users(Base):
     __tablename__ = "users"
 
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    firstName = Column(String)
+
