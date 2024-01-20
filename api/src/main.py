beb65de43cd3d94c77270c6d702eccdf6dd18ee5
JCurl08 <curl@student.ubc.ca>
2024-01-20 15:33:16 -0800

    working on schemas

--- api/src/main.py
+++ api/src/main.py
@@ -1,25 +1,35 @@
 from typing import Union, Annotated
+from sqlalchemy.orm import Session
 
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
 
+from .db import crud, models, schemas
+from .db.database import SessionLocal, engine
+
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+# TODO Authentication
 # getter for authentication process
-@app.get("/user/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+# @app.get("/user/")
+# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+#     return {"token": token}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/users/me")
+# async def read_users_me(current_user: Annotated[schemas.User, Depends(schemas.get_current_user)]):
+#     return current_user
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
+@app.post("/user")
+async def login(email: str, password: str, db: Session = Depends(get_db)):
+    user = crud.login(db, email, password)
+    return user
