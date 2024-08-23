e4d1b569774024d85dd3b93b10baa1d53f2e0268
kyleetd <kyleetd@gmail.com>
2024-08-23 13:30:38 -0700

    Updated typescript version.

--- ui/src/components/TopBar.tsx
+++ ui/src/components/TopBar.tsx
@@ -1,70 +1,80 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Logo from '../dropn-logo-horiz-dark.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Logo from "../assets/dropn-logo-horiz-dark.png";
+import { Link } from "react-router-dom";
 
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import AdbIcon from '@mui/icons-material/Adb';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import AdbIcon from "@mui/icons-material/Adb";
 
-const pages = [{ name: 'Events', path: '/events' }, { name: 'Memberships', path: '/memberships' }, { name: 'Purchases', path: '/purchases' }];
+const pages = [
+  { name: "Events", path: "/events" },
+  { name: "Memberships", path: "/memberships" },
+  { name: "Purchases", path: "/purchases" },
+];
 
 function TopBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElNav(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    return (
-      <AppBar position="static" sx={{backgroundColor: "#282828"}}>
-        <Container maxWidth="xl">
-            <Toolbar disableGutters>
-            <Link to="/">
-                <img src={Logo} alt="Logo" style={{ marginRight: '50px', width: '165px', height: '36px' }} />
-            </Link>
-        
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              LOGO
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <Link to={page.path} style={{ textDecoration: 'none' }}>
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#282828" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="Logo"
+              style={{ marginRight: "50px", width: "165px", height: "36px" }}
+            />
+          </Link>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Link to={page.path} style={{ textDecoration: "none" }}>
                 <Button
                   key={page.name}
                   onClick={handleCloseNavMenu}
-                  sx={{ mx: 2, my: 2, color: 'white', display: 'block' }}
+                  sx={{ mx: 2, my: 2, color: "white", display: "block" }}
                 >
                   {page.name}
                 </Button>
               </Link>
-              ))}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    );
-  }
-  export default TopBar;
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default TopBar;
